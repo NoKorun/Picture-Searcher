b3f697ad4c9c7967476a0c592f8c708106dd6ad7
NoKorun <45893644+NoKorun@users.noreply.github.com>
2021-06-20 16:58:52 +0500

    Update Picture_seeker.py
    Added exception when no results were found or were found less that expected.

--- Picture_seeker.py
+++ Picture_seeker.py
@@ -8,6 +8,7 @@ import pathlib
 
 manager = urllib3.PoolManager()
 searchTerm = input("Input search phrase\n")
+searchQuantity = int(input("Input number of results, max 10\n"))
 # My google API
 searchUrl = "https://www.googleapis.com/customsearch/v1?key=AIzaSyDPXV3Nj3vW3B12T4M1YKnCcrghvAFvhsE&cx=3f34f7e4dbe6af60a&q=" + searchTerm + "&searchType=image&fileType=jpg&imgSize=xlarge&alt=json"
 r = manager.request('GET', searchUrl)   # Image search
@@ -15,11 +16,18 @@ api_output = simplejson.loads(r.data.decode('utf-8'))   # Decode to json format
 
 links = []  # Links to the images
 
-for line in api_output['items']:
-    links.append(line['link'])
+try:
+    for line in api_output['items']:
+        links.append(line['link'])
+except KeyError:
+    if api_output['searchInformation']['totalResults'] == "0":
+        print("no results")
+    else:
+        print("error occurred")
+    exit()
 
 
-def download_png(url, path):    # Image saving
+def download_png(url, path):    # Image saving function
     r = requests.get(url)
     img = Image.open(io.BytesIO(r.content))     # Byte string into image
     img.save(path)
@@ -33,8 +41,10 @@ except OSError:
 else:
     print("Successfully created the directory %s " % path)
 
+if len(links) < searchQuantity:
+    print("%s pictures were requested, but only %s were found" %(searchQuantity, len(links)))
 
-for link in links:
-    hashv = str(hash(link))
+for i in range(len(links)):
+    hashv = str(hash(links[i]))
     new_path = path + hashv + ".png"
-    download_png(link, new_path)
+    download_png(links[i], new_path)
